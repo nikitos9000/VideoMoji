fd4836f892997ac9a1da5956df468e3203062d3f
Nikita Smetanin <nikita@luka.ai>
2017-04-30 10:21:30 +0300

    update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-# sudo apt-get install portaudio
-# brew install portaudio
+# linux
 
-# sudo apt-get install cmake libboost-dev
+sudo apt-get install portaudio
+sudo apt-get install cmake libboost-dev
+
+# Mac OS X
+
+brew install lua
+brew install portaudio
+
+# Common
+
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch; bash install-deps;
+./install.sh
+
+. /Users/nsmetanin/torch/install/bin/torch-activate
+
+for NAME in dpnn nn optim optnet csvigo cutorch cunn fblualib torchx tds; do
+    luarocks install ${NAME};
+done
